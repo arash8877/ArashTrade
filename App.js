01d581c696f5a66980844d4d8229764ea57ec58b
Arash <arash8877@gmail.com>
2025-06-02 09:15:26 +0200

    initialize the app

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+} from "react-native";
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{backgroundColor: 'white', flex: 1}}>
+
+      <View style={{backgroundColor: 'blue', flex: 2}}/>
+      <View style={{backgroundColor: 'gold', flex: 1}}/>
+      <View style={{backgroundColor: 'tomato', flex: 2}}/>
+     
     </View>
   );
 }
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
 });
