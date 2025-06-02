53f507e89ffdbd80ccd62917bf441899d6c86661
Arash <arash8877@gmail.com>
2025-06-02 10:51:38 +0200

    welcome page

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StyleSheet, View } from "react-native";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import { StyleSheet, View, SafeAreaView, ImageBackground, Image, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "white", flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{ backgroundColor: "blue", width: 100, height: 300, alignSelf: 'flex-end' }} />
-      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-    </View>
+    <ImageBackground
+      style={styles.container}
+      source={require("./assets/background.jpg")}
+      resizeMode="cover"
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("./assets/logo-red.png")} style={styles.logo} />
+        <Text style={styles.logoText}>This is the logo</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <View style={styles.registerBtn}/>
+        <View style={styles.loginBtn}/>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+  },
+  logoContainer: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 100,
+    left: "50%",
+    marginLeft: -50, // Half the width to center it
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+  logoText: {
+    textAlign: "center",
+    marginTop: 20,
+  },
+  buttons: {
+    width: "100%",
+    height: 200,
+    position: "absolute",
+    bottom: 0,
+    display: "flex",
+  },
+  registerBtn: {
+    width: '100%',
+    height: "50%",
+    backgroundColor: "blue",
+  },
+  loginBtn: {
+    width: '100%',
+    height: "50%",
+    backgroundColor: "red",
   },
 });
