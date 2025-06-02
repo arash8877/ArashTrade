4035d93d458a73f38dec327fb14d1e3c705f6db8
Arash <arash8877@gmail.com>
2025-06-02 09:26:45 +0200

    aligning

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-import {
-  StyleSheet,
-  View,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
-
   return (
-    <View style={{backgroundColor: 'white', flex: 1}}>
-
-      <View style={{backgroundColor: 'blue', flex: 2}}/>
-      <View style={{backgroundColor: 'gold', flex: 1}}/>
-      <View style={{backgroundColor: 'tomato', flex: 2}}/>
-     
+    <View style={{ backgroundColor: "white", flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{ backgroundColor: "blue", width: 100, height: 300, alignSelf: 'flex-end' }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
