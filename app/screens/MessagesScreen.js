1c18b45c18426f77b657e20f0602791a98924d43
Arash <arash8877@gmail.com>
2025-06-03 21:28:49 +0200

    create ListItemSeparator

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,7 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import ListItemSeparator from "../components/ListItemSeparator";
 
 const messages = [
   {
@@ -32,6 +33,7 @@ export default function MessagesScreen() {
         renderItem={({ item }) => (
           <ListItem title={item.title} subTitle={item.description} image={item.image} />
         )}
+        ItemSeparatorComponent={() => <ListItemSeparator />}
       />
     </Screen>
   );
