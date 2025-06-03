8d638bebdc2b42567461aa7333288f147e486af7
Arash <arash8877@gmail.com>
2025-06-03 10:59:34 +0200

    use icons from expo

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import { View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-     <WelcomeScreen/>
-    //  <ViewImageScreen/>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      {/* <WelcomeScreen /> */}
+      <MaterialCommunityIcons name="email" size={60} color= 'blue'/>
+      {/* <ViewImageScreen /> */}
+    </View>
   );
 }
-
-
