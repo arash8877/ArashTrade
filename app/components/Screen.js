46aa35f0fb9d4ffbba6dd0bf1c36d0b0f139335d
Arash <arash8877@gmail.com>
2025-06-03 21:18:29 +0200

    create MessageScreen

---
+++ app/components/Screen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import Constants from "expo-constants";
+
+export default function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Constants.statusBarHeight,
+  },
+});
