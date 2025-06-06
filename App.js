6c0b6ffe6a76ec005090d1b0c182083ec055f23c
Arash <arash8877@gmail.com>
2025-06-06 18:06:46 +0200

    using switch

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { TextInput, View } from "react-native";
+import { Switch, TextInput, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -12,15 +12,13 @@ import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import CustomTextInput from "./app/components/CustomTextInput";
+import { useState } from "react";
 
 export default function App() {
+  const [isNew, setIsNew] = useState(false)
   return (
     <Screen>
-      <CustomTextInput
-      // onChangeText={}
-        placeholder="First Name"
-        icon="email"
-      />
+     <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue) }/>
     </Screen>
   );
 }
