cf78820f86ec93ee9192fcea243738acfca4e9fd
Arash <arash8877@gmail.com>
2025-06-17 12:18:01 +0200

    add get data to the cache layer

--- app/api/utility/cache.js
+++ app/api/utility/cache.js
@@ -3,7 +3,9 @@
 import { AsyncStorage } from "react-native";
 
 const prefix = "cache_";
+const expireTime = 1000 * 60 * 60; // 1 hour in milliseconds
 
+//--------------- store data ----------------
 const store = async (key, value) => {
   try {
     const item = {
@@ -17,6 +19,34 @@ const store = async (key, value) => {
   }
 };
 
+//--------------- get data ----------------
+const isExpired = (item) => {
+  const now = Date.now();
+  return now - item.timestamp > expireTime;
+};
+
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(`${prefix}${key}`);
+    const item = JSON.parse(value);
+
+    if (!item) return null;
+
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(`${prefix}${key}`);
+      return null;
+    }
+
+    return item.value;
+  } catch (error) {
+    console.log("Error retrieving data:", error);
+    return null;
+  }
+};
+
+
+//--------------- export ----------------
 export default {
   store,
+  get,
 };
