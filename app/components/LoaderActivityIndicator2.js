47836cf408632682244f58f48d61d8391094a6fa
Arash <arash8877@gmail.com>
2025-06-19 13:51:10 +0200

    add overlay to the loader

--- app/components/LoaderActivityIndicator2.js
+++ app/components/LoaderActivityIndicator2.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import LottieView from "lottie-react-native";
 
 const LoaderActivityIndicator2 = ({ visible = false }) => {
   if (!visible) return null;
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require("../assets/animations/loader2.json")}
-      style={{ flex: 1 }}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require("../assets/animations/loader2.json")}
+        style={{ flex: 1 }}
+      />
+    </View>
   );
 };
 
 export default LoaderActivityIndicator2;
+
+const styles = StyleSheet.create({
+  overlay: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+    opacity: 0.8,
+    position: "absolute",
+    zIndex: 1,
+  },
+});
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -29,32 +29,34 @@ const ListingsScreen = ({ navigation }) => {
   const getListingsApi = useApi(listingsApi.getListings);
 
   useEffect(() => {
-    getListingsApi.request(1,2,3);
+    getListingsApi.request(1, 2, 3);
   }, []);
 
   return (
-    <Screen style={styles.screen}>
-      {getListingsApi.error && (
-        <>
-          <CustomText>Couldn't retrieve the listings</CustomText>
-          <CustomButton title="Retry" onPress={()=> getListingsApi.request(1, 2, 3)} />
-        </>
-      )}
+    <>
       <LoaderActivityIndicator visible={getListingsApi.loading} />
-      <FlatList
-        data={getListingsApi.data}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subtitle={"DKK" + item.price}
-            imageUrl={item.images[0]?.url}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            thumbnailUrl={item.images[0]?.thumbnailUrl}
-          />
+      <Screen style={styles.screen}>
+        {getListingsApi.error && (
+          <>
+            <CustomText>Couldn't retrieve the listings</CustomText>
+            <CustomButton title="Retry" onPress={() => getListingsApi.request(1, 2, 3)} />
+          </>
         )}
-      />
-    </Screen>
+        <FlatList
+          data={getListingsApi.data}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subtitle={"DKK" + item.price}
+              imageUrl={item.images[0]?.url}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+              thumbnailUrl={item.images[0]?.thumbnailUrl}
+            />
+          )}
+        />
+      </Screen>
+    </>
   );
 };
 
