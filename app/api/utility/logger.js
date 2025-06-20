aac490625bfa43237902ac48c3158f6da241643e
Arash <arash8877@gmail.com>
2025-06-20 13:05:25 +0200

    manage error reporting using Bugsnag

---
+++ app/api/utility/logger.js
@@ -0,0 +1,13 @@
+// import Bugsnag from "@bugsnag/expo";
+
+// const log = (error) => Bugsnag.notify(error);
+
+// export default { log, start };
+
+//------------- in the App.js file, you can initialize Bugsnag like this: ------------
+// import logger from './app/api/utility/logger';
+// logger.start();
+
+//-------------- anywhere in the app ---------------
+// instead of console.log(error)
+// use logger.log(error);
